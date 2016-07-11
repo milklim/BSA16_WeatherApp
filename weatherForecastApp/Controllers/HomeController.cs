d432ed8253cefbfa2c6b062d2ecf4b33d0ec98c6
milklim <klimbox@gmail.com>
2016-07-11 15:44:18 +0300

    02_001: task2 - Inversion of Control. Dependecy Injection.

---
+++ weatherForecastApp/Controllers/HomeController.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections.Generic;
+using System.Web.Mvc;
+using weatherForecastApp.Models;
+using weatherForecastApp.Services;
+
+namespace weatherForecastApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        private IWeatherService wService;
+        const string defaultCity = "Lviv";
+        public HomeController(IWeatherService wServiceParam)
+        {
+            wService = wServiceParam;
+        }
+
+
+        public ActionResult Index(string cityName)
+        {
+            List<string> cities = new List<string> { "Днепропетровск", "Киев", "Львов", "Одесса", "Харьков" };
+            ViewBag.cityList = cities; ;
+            return View(cities);
+        }
+
+        public ActionResult CurrentWeather(string cityName = defaultCity)
+        {
+            CurrentWeather wView = wService.GetForecast(cityName, TypeOfForecast.CurrentWeather) as CurrentWeather;
+            return View(wView);
+        }
+
+
+        public ActionResult Forecast3Days(string cityName = defaultCity)
+        {
+            WeatherForecast3DaysDetailed wView = wService.GetForecast(cityName, TypeOfForecast.For3Days) as WeatherForecast3DaysDetailed;
+            return View(wView);
+        }
+
+
+        public ActionResult Forecast7Days(string cityName = defaultCity)
+        {
+            WeatherForecast7Days wView = wService.GetForecast(cityName, TypeOfForecast.For7Days) as WeatherForecast7Days;
+            return View(wView);
+        }
+
+        [HttpPost]
+        public ActionResult PostRequestHandler(string cityName)
+        {
+            if (cityName == "") RedirectToAction("Index");
+            return RedirectToAction("CurrentWeather", cityName);
+        }
+
+        public ActionResult About()
+        {
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            return View();
+        }
+    }
+}
---
+++ weatherForecastApp/Services/WeatherServices.cs
@@ -0,0 +1,46 @@
+﻿using Ninject;
+using System;
+using weatherForecastApp.Models;
+
+namespace weatherForecastApp.Services
+{
+    public class WeatherService : IWeatherService
+    {
+        private static string appId = System.Web.Configuration.WebConfigurationManager.AppSettings["openWeatherAppId"];
+        private IRequestSender requestSender;
+        public WeatherService(IRequestSender reqSenderParam)
+        {
+            requestSender = reqSenderParam; 
+        }
+
+        public IForecast GetForecast(string cityName, TypeOfForecast type)
+        {
+            IKernel ninjectKernel = new StandardKernel();
+
+            string queryParam = string.Empty;
+            switch (type)
+            {
+                case TypeOfForecast.CurrentWeather:
+                    queryParam = "weather";
+                    ninjectKernel.Bind<IForecast>().To<CurrentWeather>();
+                    break;
+                case TypeOfForecast.For3Days:
+                    queryParam = "forecast";
+                    ninjectKernel.Bind<IForecast>().To<WeatherForecast3DaysDetailed>();
+                    break;
+                case TypeOfForecast.For7Days:
+                    queryParam = "forecast/daily";
+                    ninjectKernel.Bind<IForecast>().To<WeatherForecast7Days>();
+                    break;
+            }
+            string queryString = String.Format("http://api.openweathermap.org/data/2.5/{0}?q={1}&type=like&units=metric&lang=ru&appid={2}", queryParam, cityName, appId);
+            string response = requestSender.SendRequest(queryString);
+
+            IForecast weather = ninjectKernel.Get<IForecast>();
+            object ob = Newtonsoft.Json.JsonConvert.DeserializeObject(response, weather.GetType());
+
+            return ob as IForecast; 
+        }
+
+    }
+}
